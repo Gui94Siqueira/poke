16b50465c56b4488029f3b05040887a9fac7d22b
pokenext <guilherme94siqueira@gmail.com>
2023-04-27 16:54:19 -0300

    correcao  projeto

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.css";
 
 export async function getStaticProps() {
   const maxPokemons = 251;
-  const api = `https://pokeapi.co/api/v2/pokemon/`;
+  const api = `https://pokeapi.co/api/v2/pokemon`;
 
   const res = await fetch(`${api}/?limit=${maxPokemons}`);
 
